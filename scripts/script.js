4b87d460dc8ae4e04e814a112b5aa8afaeef6a3b
Vadim Glumakov <glumakoffv@yandex.ru>
2023-01-09 16:05:27 +0300

    Исправление ошибок

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,41 @@
 /*popup*/
 let popupBg = document.querySelector(".popup");
-let openButton = document.querySelector(".profile__edit-image");
+let openButton = document.querySelector(".profile__edit");
 let buttonClose = document.querySelector(".popup__close");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.getElementById("name");
+let jobInput = document.getElementById("about");
+let profileName = document.querySelector(".profile__name");
+let profileAboutMe = document.querySelector(".profile__about");
+
+function popupOn() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAboutMe.textContent;
+    popupBg.classList.add("popup_opened");
+}
 
 openButton.addEventListener("click", popupOn);
-function popupOn() {
-    popupBg.classList.add("popup__opened");
+
+function popupOff() {
+    popupBg.classList.remove("popup_opened");
 }
 
 buttonClose.addEventListener("click", popupOff);
 
-function popupOff() {
-    popupBg.classList.remove("popup__opened");
-}
-
 /*submit*/
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__name");
-let jobInput = document.querySelector(".popup__about-me");
-let profileName = document.querySelector(".profile__title-name");
-let profileAboutMe = document.querySelector(".profile__subtitle-about-me");
+
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAboutMe.textContent = jobInput.value;
+    popupOff();
 }
 
 formElement.addEventListener("submit", handleFormSubmit);
 
-/*При нажатии на кнопку "сохранить" закрытие попап*/
-let popupSave = document.querySelector(".popup__button");
 
-popupSave.addEventListener("click", popupOff);
+
+
+
+
